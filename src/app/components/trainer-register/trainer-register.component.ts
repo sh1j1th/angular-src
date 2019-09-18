e40b7dea89d5e1325d5b7b13bd1662d2dcb3ed16
Shijith K <shijithkappoor@gmail.com>
2019-09-18 22:37:31 +0530

    trainer backend and validation edits

--- src/app/components/trainer-register/trainer-register.component.ts
+++ src/app/components/trainer-register/trainer-register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFlashMessageService } from 'ng-flash-messages';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-trainer-register',
@@ -6,10 +10,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trainer-register.component.scss']
 })
 export class TrainerRegisterComponent implements OnInit {
+  firstName: String;
+  lastName: String;
+  contact: Number;
+  linkedInId: String;
+  email: String;
+  password: String;
+  confPassword: String;
+  tNc: boolean;
+//there is more to add here, ask abt how to deal with the arrays
 
-  constructor() { }
+  constructor(private validateService: ValidateService,
+    private flashMessage: NgFlashMessageService,
+    private http: HttpClient) { }
 
   ngOnInit() {
   }
 
-}
+  onTrainerRegisterSubmit(trainer) {
+    console.log(trainer.value)
+
+    var body = "firstName=" + trainer.firstName
+      + "&lastName=" + trainer.lastName
+      + "&contact=" + trainer.contact;
+
+    let headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+
+    this.http.post('http://localhost:3000/api/trainerRegister', body,
+      { headers: headers, responseType: 'text' }).subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error)
+      )
+
+    const user = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      contact: this.contact,
+      linkedInId: this.linkedInId,
+      email: this.email,
+      password: this.password,
+      confPassword: this.confPassword,
+      tNc: this.tNc
+    }
+    //Required Fields
+    if (this.validateService.validateTrainerRegister(user)) {
+      this.flashMessage.showFlashMessage({
+        // Array of messages each will be displayed in new line
+        messages: ["Please fill in all the fields"],
+        // Whether the flash can be dismissed by the user defaults to false
+        dismissible: true,
+        // Time after which the flash disappears defaults to 2000ms
+        timeout: 5000,
+        // Type of flash message, it defaults to info and success, warning, danger types can also be used
+        type: 'danger'
+      });
+    }
+
+    //Validate Email
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessage.showFlashMessage({
+        // Array of messages each will be displayed in new line
+        messages: ["Please fill in a valid email"],
+        // Whether the flash can be dismissed by the user defaults to false
+        dismissible: true,
+        // Time after which the flash disappears defaults to 2000ms
+        timeout: true,
+        // Type of flash message, it defaults to info and success, warning, danger types can also be used
+        type: 'danger'
+      });
+    }
+
+    //validate Terms and conditions
+    if (!this.validateService.validateTnC(user.tNc)) {
+      this.flashMessage.showFlashMessage({
+        // Array of messages each will be displayed in new line
+        messages: ["Please agree to our tnc"],
+        // Whether the flash can be dismissed by the user defaults to false
+        dismissible: true,
+        // Time after which the flash disappears defaults to 2000ms
+        timeout: true,
+        // Type of flash message, it defaults to info and success, warning, danger types can also be used
+        type: 'danger'
+      });
+    }
+  }
+
+} 
--- src/app/services/validate.service.ts
+++ src/app/services/validate.service.ts
@@ -13,6 +13,14 @@ export class ValidateService {
     } else
       return true;
   }
+
+  validateTrainerRegister(user) {
+    if (user.firstName == undefined || user.lastName == undefined || user.email == undefined || user.contact == undefined || user.password == undefined || user.confPassword == undefined) {
+      return false;
+    } else
+      return true;
+  }
+
   validateEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
