f26d49e13cdc8d2c8cb8dbe9aa0c757b5ce1e6a9
sh1j1th <shijithkappoor@gmail.com>
2019-09-25 12:46:21 +0530

    minor edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 import { LearnerDashboardComponent } from './components/learner-dashboard/learner-dashboard.component';
 import { TrainerDashboardComponent } from './components/trainer-dashboard/trainer-dashboard.component';
 import { LearnerRegisterComponent } from './components/learner-register/learner-register.component';
 import { TrainerRegisterComponent } from './components/trainer-register/trainer-register.component';
 import { HomeComponent } from './components/home/home.component';
-
+ 
 import { ValidateService } from './services/validate.service';
 import { NgFlashMessagesModule } from 'ng-flash-messages';
 
@@ -21,7 +19,6 @@ const  appRoutes: Routes = [
   { path:'', component: HomeComponent},
   { path:'learner-register', component: LearnerRegisterComponent},
   { path:'trainer-register', component: TrainerRegisterComponent},
- 
   { path:'learner-dashboard', component: LearnerDashboardComponent},
   { path:'trainer-dashboard', component: TrainerDashboardComponent}
  
@@ -31,7 +28,6 @@ const  appRoutes: Routes = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    
     LearnerDashboardComponent,
     TrainerDashboardComponent,
     LearnerRegisterComponent,
