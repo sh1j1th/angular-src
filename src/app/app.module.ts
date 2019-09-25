91b7f402566a92cd29bc868810740bc9ecf54c7b
sh1j1th <shijithkappoor@gmail.com>
2019-09-25 11:47:28 +0530

    early morning commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './components/login/login.component';
+
 import { LearnerDashboardComponent } from './components/learner-dashboard/learner-dashboard.component';
 import { TrainerDashboardComponent } from './components/trainer-dashboard/trainer-dashboard.component';
 import { LearnerRegisterComponent } from './components/learner-register/learner-register.component';
 import { TrainerRegisterComponent } from './components/trainer-register/trainer-register.component';
 import { HomeComponent } from './components/home/home.component';
-import { RegPopupComponent } from './components/reg-popup/reg-popup.component';
 
 import { ValidateService } from './services/validate.service';
 import { NgFlashMessagesModule } from 'ng-flash-messages';
@@ -22,24 +21,23 @@ const  appRoutes: Routes = [
   { path:'', component: HomeComponent},
   { path:'learner-register', component: LearnerRegisterComponent},
   { path:'trainer-register', component: TrainerRegisterComponent},
-  { path:'login', component: LoginComponent},
+ 
   { path:'learner-dashboard', component: LearnerDashboardComponent},
-  { path:'trainer-dashboard', component: TrainerDashboardComponent},
-  { path:'reg-popup', component: RegPopupComponent}
-
+  { path:'trainer-dashboard', component: TrainerDashboardComponent}
+ 
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent,
+    
     LearnerDashboardComponent,
     TrainerDashboardComponent,
     LearnerRegisterComponent,
     TrainerRegisterComponent,
-    HomeComponent,
-    RegPopupComponent
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
